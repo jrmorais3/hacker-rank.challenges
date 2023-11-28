c5eb1af3624d03270996216c193ad7ae9a212789
Junior Morais <franciscojunior@employer.com.br>
2023-11-28 12:11:31 -0300

    Add project files.

---
+++ WeekOne/BreakingTheRecords/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOne.BreakingTheRecords
+{
+    public class Result
+    {
+        public static int[] breakingRecords(List<int> scores)
+        {
+            var topRecord = scores[0];
+            var topResults = 0;
+            var lowRecord = scores[0];
+            var lowResults = 0;
+            int[] result = new int [2];
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scores[i] > topRecord)
+                {
+                    topRecord = scores[i];
+                    topResults++;
+                }
+                else if (scores[i] < lowRecord)
+                {
+                    lowRecord = scores[i];
+                    lowResults++; 
+                }
+            }
+
+            result[0] = topResults;
+            result[1] = lowResults;
+            
+            return result;
+
+        }
+    }
+}
---
+++ WeekOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WeekOne.BreakingTheRecords;
+
+namespace WeekOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            BreakingTheRecords
+
+            List<int> scores = new List<int> { 10, 5, 20, 20, 4, 5, 2, 25, 1};
+            int[] result = Result.breakingRecords(scores);
+            Console.WriteLine(String.Join(" ", result));
+            Console.ReadKey();
+            */
+        }
+    }
+}
