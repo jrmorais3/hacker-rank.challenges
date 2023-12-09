14b9d399de711a8f26050d40ae79e4293fd89ce4
Junior Morais <franciscojunior@employer.com.br>
2023-12-09 06:49:53 -0300

    SparseArrays

---
+++ WeekOne/SparseArrays/Result.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WeekOne.SparseArrays
+{
+    public class Result
+    {
+
+        /*
+         * Complete the 'matchingStrings' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. STRING_ARRAY strings
+         *  2. STRING_ARRAY queries
+         */
+
+        public static List<int> MatchingStrings(List<string> strings, List<string> queries)
+        {
+            int[] result = new int[queries.Count];
+
+            for (int i = 0; i < queries.Count; i++)
+            {
+                for (int j = 0; j < strings.Count; j++)
+                {
+                    if (strings[j].ToString().Equals(queries[i]))
+                    {
+                        result[i] += 1; 
+                    }
+                }
+            }
+
+            return result.ToList();
+
+        }
+    }
+}
